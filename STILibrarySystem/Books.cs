60a3bdc8b68a3110570c015c9b78dc679ddd6d1e
CEGA22 <71625955+CEGA22@users.noreply.github.com>
2021-01-01 15:50:04 +0800

    Homepage design

---
+++ STILibrarySystem/Books.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace STILibrarySystem
+{
+   public class Books
+    {
+        SqlConnection con = new SqlConnection(@"Data Source=DESKTOP-N1C2I03;Initial Catalog=STILibrarySystem;Integrated Security=True");
+         public int TotalBooks;
+
+         public void totalbooks()
+        {
+            try
+            {
+                SqlCommand cmd = con.CreateCommand();
+                cmd.CommandText = "SELECT COUNT(*) FROM List_of_Books";
+                con.Open();                
+                 TotalBooks = Convert.ToInt32(cmd.ExecuteScalar());
+                con.Close();
+
+            }
+
+            catch (Exception e)
+            {
+
+            }
+        }
+
+        
+        
+    }
+}
